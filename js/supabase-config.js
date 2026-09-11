bbc9ea4a83abf96de01ede417f08b98c2ffdc23c
MissyMilaS <tutubyca2007@gmail.com>
2026-09-11 14:25:50 -0500

    url database

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,7 +1,7 @@
 // Configuracion publica de Supabase.
 // Reemplaza estos valores por los de tu proyecto. Nunca coloques aqui service_role key.
-window.CLOUDTASKS_SUPABASE_URL = '';
-window.CLOUDTASKS_SUPABASE_ANON_KEY = '';
+window.CLOUDTASKS_SUPABASE_URL = 'https://cznqqhlbzmpyxzjvcfrv.supabase.co';
+window.CLOUDTASKS_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6bnFxaGxiem1weXh6anZjZnJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg5Nzk0NzQsImV4cCI6MjEwNDU1NTQ3NH0.8Ifp8CrbSDIBc4oVjex1jdudUjRjzTqsOtFaK0YfJlk';
 
 window.cloudTasksSupabase = window.CLOUDTASKS_SUPABASE_URL && window.CLOUDTASKS_SUPABASE_ANON_KEY
   ? window.supabase.createClient(
